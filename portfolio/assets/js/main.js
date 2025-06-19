3d3cc6bbfcc3f82d021368752667f5bb82aeb5f3
murilois <murilomattosdev05@gmail.com>
2025-06-18 22:22:28 -0300

    Adicionando novas imagens e ícones e continuando o JavaScript

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -1,9 +1,33 @@
+// Função para atualizar as informações do Profile
 function updateProfileInfo(profileData){
-    const picture = document.getElementById('profile.photo')
+    const picture = document.getElementById('profile-photo');
     picture.src = profileData.photo;
     picture.alt = profileData.name;
+    const name = document.getElementById('profile-name');
+    name.innerText = profileData.name;
+    const linkedin = document.getElementById('profile-linkedin');
+    linkedin.innerText = profileData.linkedin;
+    const github = document.getElementById('profile-github');
+    github.innerText = profileData.github;
+    const email = document.getElementById('profile-email');
+    email.innerText = profileData.email;
 }
-
+// Função para atualizar o central
+function updateCentralInfo(profileData){
+    const portfolio = document.getElementById('profile-portfolio');
+    portfolio.innerHTML = profileData.portfolio.map(project =>
+        `<h2>${project.name}</h2>
+            <button class="trigger" type="button">
+                Clique para ver o projeto
+            </button>
+            <div class="content">
+                    <figure>
+                        <img src="assets/imgs/projetofinal1.png" alt="Loading">
+                        <figcaption><a href="">Clique para ver o repositório do projeto</a></figcaption>
+                    </figure>
+            </div>`)
+.join('');
+}
 (async () => {
     const profileData = await fetchProfileData();
     updateProfileInfo(profileData);
